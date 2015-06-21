757663e0be98b5c0691660a069fae0691ae1dae7
Erftralle <Erftralle@users.noreply.github.com>
2015-06-21 11:12:52 +0200

    Improve the display of the star rating
    Conflicts:
    	components/com_joomgallery/views/category/tmpl/bootone_images.php
    	media/joomgallery/css/bootone.css

--- components/com_joomgallery/views/category/tmpl/bootone_images.php
+++ components/com_joomgallery/views/category/tmpl/bootone_images.php
@@ -36,7 +36,7 @@
         <div class="thumbnail">
           <a <?php echo $row->atagtitle; ?> href="<?php echo $row->link; ?>">
             <img src="<?php echo $row->thumb_src; ?>" <?php echo $row->imgwh; ?> alt="<?php echo $row->imgtitle; ?>" /></a>
-          <div class="caption jg_catelem_txt">
+          <div class="caption">
             <ul class="list-unstyled text-center">
 <?php     if($this->_config->get('jg_showtitle') || ($this->_config->get('jg_showpicasnew') && $row->isnew)): ?>
               <li>
@@ -156,3 +156,4 @@
     <?php echo $this->pagination->getPagesLinks(); ?>
   </div>
 <?php endif;
+
